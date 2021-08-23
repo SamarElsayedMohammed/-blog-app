74046faaa0c91c8e9f0963d542868ff5104d8e2b
SamarElsayedMohammed <samarelsayedmohammed12345@gmail.com>
2021-08-23 04:16:06 +0200

    initial commit

---
+++ blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class Post(models.Model):
+    """Model definition for Post."""
+
+    # TODO: Define fields here
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
+
